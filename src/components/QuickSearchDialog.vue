4be07d0db92d3496ef959b8cd8803cf940d4f51d
Ambitious <1535820771@qq.com>
2022-09-26 20:54:45 +0800

    add:快搜增加关键词搜索历史匹配

--- src/components/QuickSearchDialog.vue
+++ src/components/QuickSearchDialog.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="quick-search-dialog-wrap">
+<div ref="curNodeRef" class="quick-search-dialog-wrap">
   <el-dialog 
     v-model="visible" 
     :show-close="false"
@@ -37,10 +37,16 @@
           @click="handleSearch"
           icon="Search">搜 索</el-button>
       </div>
-      <el-input
+      <el-autocomplete
         v-model="searchKey"
+        style="width:100%"
+        ref="searchInputRef"
+        :teleported="false"
+        highlight-first-item
+        select-when-unmatched
+        :fetch-suggestions="querySearch"
         placeholder="输入要搜索视频的关键词（按下回车键可快速搜索）"
-        @keydown.enter="handleSearch"
+        @select="handleSearch"
       />
     </div>
   </el-dialog>
@@ -59,6 +65,9 @@ const searchKey = ref('')
 // 要搜索的视频网站地址
 const searchSite = ref('')
 
+// 搜索框实例
+const searchInputRef = ref()
+
 // 可选的网站
 const sites = ref([
   { label: '腾讯视频', url: 'https://v.qq.com/x/search/?q=${searchKey}' },
@@ -73,6 +82,32 @@ const show = () => {
   visible.value = true
 }
 
+// 获取搜索缓存
+const getSearchHistory = () => {
+  return JSON.parse(localStorage.getItem('search-history') || '[]') || []
+}
+
+// 加载自动补全搜索项
+const querySearch = (str = '', cb = () => {}) => {
+  // 1 拉取搜素缓存
+  const searchHistory = getSearchHistory()
+  if (searchHistory.length === 0) {
+    return cb([])
+  }
+  // 2 匹配 5 个选项
+  const res = new Array()
+  searchHistory.some(v => {
+    if (v.toLowerCase().indexOf(str.toLowerCase()) !== -1) {
+      res.push(v)
+      if (res.length >= 5) {
+        return true
+      }
+    }
+    return false
+  })
+  return cb(res.map(v => ({value: v})))
+}
+
 // 搜索资源
 const handleSearch = () => {
   const site = searchSite.value
@@ -80,6 +115,24 @@ const handleSearch = () => {
   if (!site || !key) {
     return
   }
+  // 保存搜索关键词
+  const searchHistory = getSearchHistory()
+  searchHistory.unshift(key)
+  // 过滤掉重复的关键词
+  const distinctSet = new Set()
+  for (let i = 0; i < searchHistory.length;) {
+    if (distinctSet.has(searchHistory[i])) {
+      searchHistory.splice(i, 1)
+    } else {
+      distinctSet.add(searchHistory[i])
+      i++
+    }
+  }
+  // 防止缓存数组过长
+  while (searchHistory.length > 100) {
+    searchHistory.pop()
+  }
+  localStorage.setItem('search-history', JSON.stringify(searchHistory))
   window.open(site.replace('${searchKey}', key), '_blank')
   setTimeout(() => {
     close()
@@ -87,6 +140,7 @@ const handleSearch = () => {
 }
 
 const close = () => {
+  searchInputRef.value.close()
   visible.value = false
   searchSite.value = ''
   searchKey.value = ''
